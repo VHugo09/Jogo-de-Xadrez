fc61efb73624ed832482f0a66670c00b9768d499
VHugo09 <vitorhugo.mb.007@gmail.com>
2022-06-24 19:15:15 -0300

    First Class

--- scr/application/Program.java
+++ scr/application/Program.java
@@ -1,5 +1,14 @@
 package scr.application;
 
+import scr.boardgame.Position;
+
 public class Program {
+
+    public static void main(String[]args) {
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+        
+    }
     
 }
